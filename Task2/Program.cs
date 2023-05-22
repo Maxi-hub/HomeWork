4ecebb43caa1f07c63b5d867461f99eb2ef174e4
Emma <em.lebedeva85@gmail.com>
2023-05-22 09:53:25 +0300

    Create Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 5;
+int b = 7;
+int a1 = 2; 
+int b1 = 10;
+int a2 = -9; 
+int b2 = -3;
+
+int max = a;
+if (b > max) max = b;
+Console.WriteLine(max);
+
+int max1 = a1;
+if (b1 > max1) max1 = b1;
+Console.WriteLine(max1);
+
+int max2 = a2;
+if (b2 > max2) max2 = b2;
+Console.WriteLine(max2);
