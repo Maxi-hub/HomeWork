112d86aafba12ca11c1aa6dffdfe9962b0a3d736
Emma <em.lebedeva85@gmail.com>
2023-05-22 13:32:59 +0300

    Create Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число:" );
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int result = (number % 2);
+
+if(result == 0) Console.WriteLine("true"); 
+else Console.WriteLine("false"); 
+
+
